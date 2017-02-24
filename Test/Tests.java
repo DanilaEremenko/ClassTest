d797fe333eeb6e44cc89a29a4456549b7e6225d1
Илья Калинин < >
2017-02-24 21:14:34 +0300

    Initial commit

---
+++ Test/Tests.java
@@ -0,0 +1,12 @@
+import  org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Created by 801674 on 24.02.2017.
+ */
+public class Tests {
+    @Test
+    public void sum(){
+        assertEquals(10,Proekt.sum(2,8));
+
+    }
+}
---
+++ src/Proekt.java
@@ -0,0 +1,8 @@
+/**
+ * Created by 801674 on 24.02.2017.
+ */
+public class Proekt {
+    public static int sum(int a,int b){
+        return a+b;
+    }
+}
