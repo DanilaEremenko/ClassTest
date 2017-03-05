ec55c64a6ba0e60ab157c9d2720a8b85df1bd384
daddad <adsdas>
2017-03-05 10:20:06 +0300

    Сделано сложение, умножение и вычитание.

--- Test/Tests.java
+++ Test/Tests.java
@@ -1,14 +1,24 @@
-//import  org.junit.Test;
-//import static org.junit.Assert.*;
-///**
-// * Created by 801674 on 24.02.2017.
-// */
-//public class Tests {
-//    Proekt proekt=new Proekt(98);
-//    @Test
-//    public void sum(){
-//        assertEquals("99",proekt.sum(new Proekt("1")));
-//
-//    }
-//
-//}
+import  org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Created by 801674 on 24.02.2017.
+ */
+public class Tests {
+Proekt proekt=new Proekt("");
+    @Test
+    public void suma(){
+        assertEquals("885616141",proekt.suma(new Proekt("884659354"),new Proekt("956787")));
+
+    }
+    @Test
+    public  void proizv(){
+        assertEquals("64968696182621",proekt.proizv(new Proekt("95689"),new Proekt("678956789")));
+
+    }
+    @Test
+    public void subtraction(){
+        assertEquals("3326370759",proekt.subtraction(new Proekt("3405350345"),new Proekt("78979586")));
+    }
+
+
+}
--- src/Proekt.java
+++ src/Proekt.java
@@ -1,6 +1,7 @@
 /**
  * Created by 801674 on 24.02.2017.
  */
+
 import com.sun.org.apache.regexp.internal.RE;
 
 import java.math.BigDecimal;
@@ -11,28 +12,325 @@ import java.math.BigInteger;
  */
 
 public class Proekt {
-    public static void main(String[] args) {
+    String digit;
 
-        String str = "896745788945";
-        String str2 = "769905689706065";
-        String RezultSum=suma(str, str2);
-        System.out.println("Сумма равна = "+RezultSum);
-        String RezultProizv=proizv(str,str2);
-        System.out.println("Произведение равно = "+RezultProizv);
+    public Proekt(String strDigit) {
+        this.digit = strDigit;
+    }
+
+    public Proekt(int digit) {
+        this(String.valueOf(digit));
+    }
+
+
+
+
+
+
+
+
+    public String subtraction(Proekt str, Proekt str2) {
+        StringBuilder strBuilder = new StringBuilder(str.digit);
+        StringBuilder strBuilder2 = new StringBuilder(str2.digit);
+        StringBuilder RezultSumbuilder = new StringBuilder("");
+
+
+        if (strBuilder2.length() > strBuilder.length()) {
+            strBuilder = new StringBuilder(str2.digit);
+            strBuilder2 = new StringBuilder(str.digit);
+        }
+
+        strBuilder.reverse();
+        strBuilder2.reverse();
+
+        //Подведение чисел под общий знаменатель
+        if (strBuilder.length() != strBuilder2.length()) {
+
+            while (strBuilder.length() > strBuilder2.length()) {
+                strBuilder2.append(0);
+            }
+
+            while (strBuilder2.length() > strBuilder.length()) {
+                strBuilder.append(0);
+            }
+
+        }
+
+        int[] massInt = new int[strBuilder.length()];
+        char[] massChar = new char[strBuilder.length()];
+        int[] massInt2 = new int[strBuilder.length()];
+        char[] massChar2 = new char[strBuilder.length()];
+        int ost = 0;
+
+        for (int i = 0; i < (strBuilder.length()); i++) {
+            massChar[i] = strBuilder.charAt(i);
+            massInt[i] = Character.getNumericValue(massChar[i]);
+            massChar2[i] = strBuilder2.charAt(i);
+            massInt2[i] = Character.getNumericValue(massChar2[i]);
+        }
+        for (int i = 0; i < (strBuilder.length()); i++) {
+            int sum = 0;
+            if (massInt[i]>massInt2[i]) {
+                sum = massInt[i] - massInt2[i];
+            } if(massInt[i]<massInt2[i]) {
+                massInt[i+1]=massInt[i+1]-1;
+                sum = massInt[i]+10 - massInt2[i];
+            }
+
+            String a = Integer.toString(sum);
+            RezultSumbuilder.append(a);
+
 
     }
 
 
-    static String suma(String str, String str2) {
+        RezultSumbuilder.reverse();
+        String RezultSum = RezultSumbuilder.toString();
+        RezultSum = RezultSum.replaceFirst ("^0*", "");
+        return RezultSum;
+
+    }
+
+    public String suma(Proekt str, Proekt str2) {
         // Создание strBuilder и strBuilder2
-        StringBuilder strBuilder = new StringBuilder(str);
-        StringBuilder strBuilder2 = new StringBuilder(str2);
-        if(str2.length()>str.length()){strBuilder = new StringBuilder(str2);
-            strBuilder2 = new StringBuilder(str);}
+        StringBuilder strBuilder = new StringBuilder(str.digit);
+        StringBuilder strBuilder2 = new StringBuilder(str2.digit);
+        if (strBuilder2.length() > strBuilder.length()) {
+            strBuilder = new StringBuilder(str2.digit);
+            strBuilder2 = new StringBuilder(str.digit);
+        }
 
         strBuilder.reverse();
         strBuilder2.reverse();
-        StringBuilder RezultSumbuilder=new StringBuilder("");
+        StringBuilder RezultSumbuilder = new StringBuilder("");
+        //Подведение чисел под общий знаменатель
+        if (strBuilder.length() != strBuilder2.length()) {
+
+            while (strBuilder.length() > strBuilder2.length()) {
+                strBuilder2.append(0);
+            }
+
+            while (strBuilder2.length() > strBuilder.length()) {
+                strBuilder.append(0);
+            }
+
+        }
+
+
+        int[] massInt = new int[strBuilder.length()];
+        char[] massChar = new char[strBuilder.length()];
+        int[] massInt2 = new int[strBuilder.length()];
+        char[] massChar2 = new char[strBuilder.length()];
+        int ost = 0;
+//Сложение массивов
+        for (int i = 0; i < (strBuilder.length()); i++) {
+            massChar[i] = strBuilder.charAt(i);
+            massInt[i] = Character.getNumericValue(massChar[i]);
+            massChar2[i] = strBuilder2.charAt(i);
+            massInt2[i] = Character.getNumericValue(massChar2[i]);
+            int sum = 0;
+            if (ost == 0) {
+                sum = massInt[i] + massInt2[i];
+                ost = sum / 10;
+                sum = sum % 10;
+            } else {
+                sum = massInt[i] + massInt2[i] + ost;
+                ost = sum / 10;
+                sum = sum % 10;
+            }
+
+            String a = Integer.toString(sum);
+            RezultSumbuilder.append(a);
+
+            /*System.out.println("Сумма в разряде равна" + sum);*/
+        }
+        if (ost != 0) {
+            String a = Integer.toString(ost);
+            RezultSumbuilder.append(a);
+        }
+        //Вывод ответа в сравнении с подсчетом через BigInteger
+        RezultSumbuilder.reverse();
+        String RezultSum = RezultSumbuilder.toString();
+        RezultSum = RezultSum.replaceFirst ("^0*", "");
+        return RezultSum;
+    }
+
+
+    public String proizv(Proekt str, Proekt str2) { /*П
+        Р
+        О
+        И
+        З
+        В
+        Е
+        Д
+        Е
+        Н
+        И
+        Е
+         */
+        /*
+        System.out.println("Первый множитель"+str);
+        System.out.println("Второй множитель"+str2);
+        */
+        StringBuilder strBuilder = new StringBuilder(str.digit);
+        StringBuilder strBuilder2 = new StringBuilder(str2.digit);
+
+        if (strBuilder.length() > strBuilder2.length()) {
+
+            while (strBuilder.length() > strBuilder2.length()) {
+                strBuilder2.reverse();
+                strBuilder2.append(0);
+                strBuilder2.reverse();
+            }
+        }
+        if (strBuilder2.length() > strBuilder.length()) {
+
+            while (strBuilder2.length() > strBuilder.length()) {
+                strBuilder.reverse();
+                strBuilder.append(0);
+                strBuilder.reverse();
+            }
+        }
+/*
+        System.out.println("Первый множитель после уравнивания"+strBuilder);
+        System.out.println("Второй множитель после уравнивания"+strBuilder2);
+*/
+        //Подведение чисел под общий знаменатель
+
+        StringBuilder[] stringBuilderslog = new StringBuilder[strBuilder.length()];
+        for (int i = 0; i < strBuilder.length(); i++) {
+            stringBuilderslog[i] = new StringBuilder("");
+        }
+
+
+//Получаем составные части произведения которые потом нужно сложить
+        char massChar[] = new char[strBuilder.length()];
+        char massChar2[] = new char[strBuilder.length()];
+        int massInt[] = new int[strBuilder.length()];
+        int massInt2[] = new int[strBuilder.length()];
+        int proizv = 0;
+        int ost = 0;
+
+
+        for (int i = strBuilder.length() - 1; i >= 0; i--) {
+
+            for (int j = strBuilder.length() - 1; j >= 0; j--) {
+                massChar[j] = strBuilder.charAt(j);
+                massInt[j] = Character.getNumericValue(massChar[j]);
+                massChar2[i] = strBuilder2.charAt(i);
+                massInt2[i] = Character.getNumericValue(massChar2[i]);
+
+                if (ost == 0) {
+
+                    proizv = massInt[j] * massInt2[i];
+                    ost = proizv / 10;
+                    proizv = proizv % 10;
+
+                } else {
+
+                    proizv = massInt[j] * massInt2[i] + ost;
+                    ost = proizv / 10;
+                    proizv = proizv % 10;
+
+                }
+
+                String a = Integer.toString(proizv);
+                stringBuilderslog[i].append(a);
+
+
+            }
+
+
+            int k = i;
+            if (ost == 0) {
+                stringBuilderslog[i].reverse();
+                while (k < (strBuilder.length() - 1)) {
+                    k++;
+                    stringBuilderslog[i].append("0");
+                }
+            } else {
+                stringBuilderslog[i].append(ost);
+                stringBuilderslog[i].reverse();
+                while (k < (strBuilder.length() - 1)) {
+                    k++;
+                    stringBuilderslog[i].append("0");
+                }
+
+            }
+            ost = 0;
+
+
+        }
+//Получили составные части произведения
+
+
+/*
+        for(int i=strBuilder.length()-1;i>=0;i--){
+            System.out.println((strBuilder.length() - i) +"stringBuilderslog перед уравнением = "+stringBuilderslog[i]);
+        }
+*/
+
+        //  Подведение чисел под общий знаменатель
+        int max = 0;
+        for (int i = 0; i < strBuilder.length(); i++) {
+            if (max < stringBuilderslog[i].length())
+                max = stringBuilderslog[i].length();
+        }
+
+        for (int i = 0; i < strBuilder.length(); i++) {
+            if (stringBuilderslog[i].length() < max) {
+                while (stringBuilderslog[i].length() < max) {
+                    stringBuilderslog[i].reverse();
+                    stringBuilderslog[i].append(0);
+                    stringBuilderslog[i].reverse();
+                }
+            }
+
+        }
+        String stringslog[] = new String[strBuilder.length()];
+
+
+        for (int i = strBuilder.length() - 1; i >= 0; i--) {
+            stringslog[i] = stringBuilderslog[i].toString();
+            /*System.out.println((strBuilder.length() - i) + " Слогаемое = " + stringslog[i]);*/
+        }
+
+        String RezultProizv = "0";
+        String zero2;
+        for (int i = 0; i < strBuilder.length(); i++) {
+            zero2 = suma(RezultProizv, stringslog[i]);
+            RezultProizv = zero2;
+        }
+        RezultProizv = RezultProizv.replaceFirst ("^0*", "");
+        return RezultProizv;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public String suma(String str, String str2) {
+        // Создание strBuilder и strBuilder2
+        StringBuilder strBuilder = new StringBuilder(str);
+        StringBuilder strBuilder2 = new StringBuilder(str2);
+        if (strBuilder2.length() > strBuilder.length()) {
+            strBuilder = new StringBuilder(str2);
+            strBuilder2 = new StringBuilder(str);
+        }
+
+        strBuilder.reverse();
+        strBuilder2.reverse();
+        StringBuilder RezultSumbuilder = new StringBuilder("");
         //Подведение чисел под общий знаменатель
         if (strBuilder.length() != strBuilder2.length()) {
 
@@ -88,186 +386,70 @@ public class Proekt {
         }
         //Вывод ответа в сравнении с подсчетом через BigInteger
         RezultSumbuilder.reverse();
-        String RezultSum=RezultSumbuilder.toString();
-        BigInteger b1 = new BigInteger("999");
-        BigInteger b2 = new BigInteger("1");
-        /*System.out.println("Подсчет через BigInteger = " + b1.add(b2));*/
+        String RezultSum = RezultSumbuilder.toString();
+        RezultSum = RezultSum.replaceFirst ("^0*", "");
         return RezultSum;
     }
 
-
-
-
-
-
-    static String proizv(String str,String str2)
-    { /*П
-        Р
-        О
-        И
-        З
-        В
-        Е
-        Д
-        Е
-        Н
-        И
-        Е
-         */
-        /*
-        System.out.println("Первый множитель"+str);
-        System.out.println("Второй множитель"+str2);
-        */
+    public String subtraction(String str, String str2) {
         StringBuilder strBuilder = new StringBuilder(str);
         StringBuilder strBuilder2 = new StringBuilder(str2);
+        StringBuilder RezultSumbuilder = new StringBuilder("");
 
-        if (strBuilder.length() > strBuilder2.length()) {
+
+        if (strBuilder2.length() > strBuilder.length()) {
+            strBuilder = new StringBuilder(str2);
+            strBuilder2 = new StringBuilder(str);
+        }
+
+        strBuilder.reverse();
+        strBuilder2.reverse();
+
+        //Подведение чисел под общий знаменатель
+        if (strBuilder.length() != strBuilder2.length()) {
 
             while (strBuilder.length() > strBuilder2.length()) {
-                strBuilder2.reverse();
                 strBuilder2.append(0);
-                strBuilder2.reverse();
             }
-        }
-        if(strBuilder2.length()>strBuilder.length()){
 
             while (strBuilder2.length() > strBuilder.length()) {
-                strBuilder.reverse();
                 strBuilder.append(0);
-                strBuilder.reverse();
-            }
-        }
-/*
-        System.out.println("Первый множитель после уравнивания"+strBuilder);
-        System.out.println("Второй множитель после уравнивания"+strBuilder2);
-*/
-        //Подведение чисел под общий знаменатель
-
-        StringBuilder[] stringBuilderslog = new StringBuilder[strBuilder.length()];
-        for (int i = 0; i < strBuilder.length(); i++) {
-            stringBuilderslog[i] = new StringBuilder("");
-        }
-
-
-//Получаем составные части произведения которые потом нужно сложить
-        char massChar[]=new char[strBuilder.length()];
-        char massChar2[]=new char[strBuilder.length()];
-        int massInt[]=new int[strBuilder.length()];
-        int massInt2[]=new int[strBuilder.length()];
-        int proizv = 0;
-        int ost=0;
-
-
-
-
-
-
-
-
-        for (int i = strBuilder.length()-1; i>=0; i--) {
-
-            for (int j= strBuilder.length()-1; j >=0; j--) {
-                massChar[j] = strBuilder.charAt(j);
-                massInt[j] = Character.getNumericValue(massChar[j]);
-                massChar2[i] = strBuilder2.charAt(i);
-                massInt2[i] = Character.getNumericValue(massChar2[i]);
-
-                if (ost == 0) {
-
-                    proizv = massInt[j] * massInt2[i];
-                    ost = proizv / 10;
-                    proizv = proizv % 10;
-
-                } else {
-
-                    proizv = massInt[j] * massInt2[i] + ost;
-                    ost = proizv / 10;
-                    proizv = proizv % 10;
-
-                }
-
-                String a=Integer.toString(proizv);
-                stringBuilderslog[i].append(a);
-
-
-            }
-
-
-            int k=i;
-            if(ost==0) {
-                stringBuilderslog[i].reverse();
-                while(k<(strBuilder.length()-1)) {
-                    k++;
-                    stringBuilderslog[i].append("0");
-                }
-            }
-
-            else{
-                stringBuilderslog[i].append(ost);
-                stringBuilderslog[i].reverse();
-                while(k<(strBuilder.length()-1)) {
-                    k++;
-                    stringBuilderslog[i].append("0");
-                }
-
-            }
-            ost=0;
-
-
-
-
-
-
-
-
-
-
-        }
-//Получили составные части произведения
-
-
-/*
-        for(int i=strBuilder.length()-1;i>=0;i--){
-            System.out.println((strBuilder.length() - i) +"stringBuilderslog перед уравнением = "+stringBuilderslog[i]);
-        }
-*/
-
-        //  Подведение чисел под общий знаменатель
-        int max = 0;
-        for (int i = 0; i < strBuilder.length(); i++) {
-            if (max < stringBuilderslog[i].length())
-                max = stringBuilderslog[i].length();
-        }
-
-        for (int i = 0; i < strBuilder.length(); i++) {
-            if (stringBuilderslog[i].length() < max) {
-                while (stringBuilderslog[i].length() < max) {
-                    stringBuilderslog[i].reverse();
-                    stringBuilderslog[i].append(0);
-                    stringBuilderslog[i].reverse();
-                }
             }
 
         }
-        String stringslog[]=new String[strBuilder.length()];
+
+        int[] massInt = new int[strBuilder.length()];
+        char[] massChar = new char[strBuilder.length()];
+        int[] massInt2 = new int[strBuilder.length()];
+        char[] massChar2 = new char[strBuilder.length()];
+        int ost = 0;
+
+        for (int i = 0; i < (strBuilder.length()); i++) {
+            massChar[i] = strBuilder.charAt(i);
+            massInt[i] = Character.getNumericValue(massChar[i]);
+            massChar2[i] = strBuilder2.charAt(i);
+            massInt2[i] = Character.getNumericValue(massChar2[i]);
+        }
+        for (int i = 0; i < (strBuilder.length()); i++) {
+            int sum = 0;
+            if (massInt[i]>massInt2[i]) {
+                sum = massInt[i] - massInt2[i];
+            } if(massInt[i]<massInt2[i]) {
+                massInt[i+1]=massInt[i+1]-1;
+                sum = massInt[i]+10 - massInt2[i];
+            }
+
+            String a = Integer.toString(sum);
+            RezultSumbuilder.append(a);
 
 
-        for(int i=strBuilder.length()-1;i>=0;i--)
-        {
-            stringslog[i]=stringBuilderslog[i].toString();
-            /*System.out.println((strBuilder.length() - i) + " Слогаемое = " + stringslog[i]);*/
         }
 
-        String RezultProizv="0";
-        String zero2;
-        for (int i=0;i<strBuilder.length();i++){
-            zero2=suma(RezultProizv,stringslog[i]);
-            RezultProizv=zero2;
-        }
-        return RezultProizv;
 
-
-
+        RezultSumbuilder.reverse();
+        String RezultSum = RezultSumbuilder.toString();
+        RezultSum = RezultSum.replaceFirst ("^0*", "");
+        return RezultSum;
 
     }
 
